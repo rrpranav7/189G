077d0e7fe6573ee86b57b242da7dedfed0416c2f
rrpranav7 <123920589+rrpranav7@users.noreply.github.com>
2023-02-22 14:32:39 -0800

    #15

---
+++ base/stage_3_code/Dataset_Loader_CIFAR.py
@@ -0,0 +1,42 @@
+'''
+Concrete IO class for a specific dataset
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+import pickle
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from base.base_class.dataset import dataset
+
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_file_name = None
+
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+
+    def load(self):
+        print('loading data...')
+        f = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'rb')
+        data = pickle.load(f)
+        f.close()
+        print('training set size:', len(data['train']), 'testing set size:', len(data['test']))
+        X, y = [], []
+        for pair in data['train']:
+            #pa = np.array(pair['image'])
+            #pa = np.dot(pa[..., :3], [0.2989, 0.5870, 0.1140])
+            X.append(pair['image'])
+            y.append(pair['label'])
+
+        X = torch.from_numpy(np.array(X)).float()
+        X= X.permute(0,3, 1, 2)
+        X, y = X.unsqueeze(1), np.array(y)-1 #unsqueeze(1)
+
+
+        # Use grayscale_image_tensor in your model for prediction
+        return {'X': X, 'y': y}
